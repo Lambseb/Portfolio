0ca8bcf503cb6eb89fad5912bb9d069c580b4aff
Sébastien LAMBRANCA <116831255+Lambseb@users.noreply.github.com>
2023-10-05 17:44:20 +0200

    update add in img files in main branch and few update for responsive

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // faire le bouton qui affiche la nav vertical en responsive
-let responsiveBtn = document.querySelector('.btn_responsive_svg')
-let verticalNav = document.querySelector('.list_menu')
-console.log(responsiveBtn);
-responsiveBtn.addEventListener('click', ()=> {
+const responsiveBtn = document.querySelector('.btn_responsive_svg')
+const verticalNav = document.querySelector('.list_menu')
+
+responsiveBtn.addEventListener('click', () => 
   verticalNav.style.display = 'block',
   verticalNav.style.transition ="2s",
-  console.log(verticalNav)
-});
+);
 // script pour écrire visuellement sur  la class "text_typing" 
 //selectionner le text
 
-let typingElement = document.querySelector('.typing_selection');
+const typingElement = document.querySelector('.typing_selection');
 const textToType = "Développeur web front-end";
 let index = 0;
 
@@ -28,40 +27,26 @@ function typeText() {
 // Appelez la fonction pour commencer l'effet d'écriture
 const typingInterval = setInterval(typeText, 50);
 
-// INTERSECTION OBSERVER     
-    const ratio = 0.5;
-const options = {
-    root: null,
-    rootMargin: "0px",
-    threshold: ratio
-};
-const objectObs = document.querySelectorAll('.slider_class');
 
-function handleIntersect (entries){
-  //   console.log(entries);
-    entries.forEach( entry => {
-     if (entry.intersectionRatio > ratio) {
-         entry.target.classList.add('pics_visible');
-         observer.unobserve(entry.target);
-     }
-})}
-const observer = new IntersectionObserver(handleIntersect, options);
-objectObs.forEach(imageScrolled =>{
-    observer.observe(imageScrolled)
-})
-//  let funWrite = () => {
-//     if(writing.length > 0){
-//     console.log(writing)
-//     writing.pop()
-//     }
-//     }; FONCTION FLECHEE
-    // setTimeout(() => {
-    //     writing.pop();
-    //     console.log("reboot");
-    //   }, "1000");
 
-// retirer lettre par lettre avec un timer
+// // INTERSECTION OBSERVER     
+//     const ratio = 0.5;
+// const options = {
+//     root: null,
+//     rootMargin: "0px",
+//     threshold: ratio
+// };
+// const objectObs = document.querySelectorAll('.slider_class');
 
-// faire revenir le lettre avec un timer
-
-// fonction Intersection Observer
+// function handleIntersect (entries){
+//   //   console.log(entries);
+//     entries.forEach( entry => {
+//      if (entry.intersectionRatio > ratio) {
+//          entry.target.classList.add('pics_visible');
+//          observer.unobserve(entry.target);
+//      }
+// })}
+// const observer = new IntersectionObserver(handleIntersect, options);
+// objectObs.forEach(imageScrolled =>{
+//     observer.observe(imageScrolled)
+// })
